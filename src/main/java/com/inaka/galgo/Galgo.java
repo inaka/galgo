643cb3ae2a95968021144906b7e61f8537420bb2
alberthynek <alberthynek@gmail.com>
2014-11-25 15:56:49 -0800

    whitespace

--- src/main/java/com/inaka/galgo/Galgo.java
+++ src/main/java/com/inaka/galgo/Galgo.java
@@ -77,7 +77,7 @@ public class Galgo {
             }
 
             @Override
-            public void onServiceDisconnected(ComponentName c) {
+            	public void onServiceDisconnected(ComponentName c) {
             }
         };
 
