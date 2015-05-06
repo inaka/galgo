33ed5bef6d3fc33b2ce5d2156f109d08e16bbce0
Guillermo Rivero <guillermo@inakanetworks.com>
2015-05-06 18:01:43 -0300

    [#1] JUnit added to project. Test suite working with mock values

---
+++ src/main/java/com/inaka/galgo/GalgoTest.java
@@ -0,0 +1,21 @@
+package com.inaka.galgo;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by guillea on 5/6/15.
+ */
+public class GalgoTest extends TestCase {
+
+    public void testEnable() throws Exception {
+        assertTrue("Service Enabled", true);
+    }
+
+    public void testDisable() throws Exception {
+        assertTrue("Service Disabled", true);
+    }
+
+    public void testLog() throws Exception {
+        assertTrue("Log Working", true);
+    }
+}
